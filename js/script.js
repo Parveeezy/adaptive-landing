2eeefd256a2019dbab628eedc0c5a943811674fc
Parviz Shirinov <parveeezy@gmail.com>
2022-07-14 14:17:41 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instancesSidenav = M.Sidenav.init(sidenav);
+
+    var modal = document.querySelectorAll('.modal');
+    var instancesModal = M.Modal.init(modal);
+
+    var media = document.querySelectorAll('.materialboxed');
+    var instancesMedia = M.Materialbox.init(media);
   });
+
+ 
